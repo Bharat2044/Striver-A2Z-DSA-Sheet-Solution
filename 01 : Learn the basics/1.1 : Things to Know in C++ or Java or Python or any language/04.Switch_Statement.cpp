da9825e74a9ad2b7988842e03eadce7e76680df9
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-26 18:59:40 +0000

    code added

---
+++ 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/04.Switch_Statement.cpp
@@ -0,0 +1,17 @@
+// Link : https://practice.geeksforgeeks.org/problems/java-switch-case-statement3529/1
+
+class Solution{
+    static double switchCase(int choice, List<Double> arr){
+        // code here
+        switch(choice){
+            case 1:
+                return Math.PI * arr.get(0) * arr.get(0);
+            case 2:
+                return arr.get(0) * arr.get(1);
+        }
+        return 0;
+    }
+}
+
+// Time Complexity = O(1)
+// Space Complexity = O(1)
