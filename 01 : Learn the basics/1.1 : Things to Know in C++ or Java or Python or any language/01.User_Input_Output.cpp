e74f0953445fa2ca15715d596f9bb0fb48d8659b
Bharat2044 <104676718+Bharat2044@users.noreply.github.com>
2023-02-26 18:24:08 +0000

    code added

---
+++ 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/01.User_Input_Output.cpp
@@ -0,0 +1,12 @@
+// Link : https://practice.geeksforgeeks.org/problems/c-input-output2432/1
+
+class Solution{   
+public:
+    int multiplication(int A, int B){
+        // code here 
+        return A*B;
+    }
+};
+
+// Time Complexity = O(1)
+// Space Complexity = O(1)
