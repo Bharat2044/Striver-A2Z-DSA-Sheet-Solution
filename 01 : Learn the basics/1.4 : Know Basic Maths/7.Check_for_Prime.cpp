d7c527a1e2431646f12885643fa1e9b25207fd61
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-03-18 16:24:43 +0000

    code added

---
+++ 01 : Learn the basics/1.4 : Know Basic Maths/7.Check_for_Prime.cpp
@@ -0,0 +1,49 @@
+// Link - gfg : https://practice.geeksforgeeks.org/problems/prime-number2314/1
+
+
+class Solution{
+public:
+    // Approach - 1 :  Time Complexity = O(n), Space Complexity = O(1)
+    /*int isPrime(int n) {
+        if(n <= 1)
+            return 0;
+            
+        for(int i=2; i<n; i++) {
+            if(n%i == 0)
+                return 0;
+        }
+        
+        return 1;
+    }*/
+    
+    // Approach - 2 :  Time Complexity = O(sqrt(n)), Space Complexity = O(1)
+    /*int isPrime(int n) {
+        if(n <= 1)
+            return 0;
+            
+        for(int i=2; i*i<=n; i++) {
+            if(n%i == 0)
+                return 0;
+        }
+        
+        return 1;
+    }*/
+    
+     // Approach - 3 :  Time Complexity = O(sqrt(n)), Space Complexity = O(1)
+    int isPrime(int n) {
+        if(n <= 1)
+            return 0;
+    
+        if(n == 2 || n == 3)
+            return 1;
+    
+        if(n%2 == 0 || n%3 == 0)
+            return 0;
+    
+        for(int i=5; i*i<=n; i+=6){
+            if(n%i==0 || n%(i+2)==0)
+                return 0;
+        }
+        return 1;
+    }
+};
