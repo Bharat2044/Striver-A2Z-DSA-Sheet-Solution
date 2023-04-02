d1c230a08c73243e92554cba6c5bea2f1be53d70
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-04-02 17:14:20 +0000

    code added

--- 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/07.While_loops.cpp
+++ 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/07.While_loops.cpp
@@ -1,14 +1,35 @@
 // Link : https://practice.geeksforgeeks.org/problems/while-loop-printtable/1
 
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution{
     public:
-        void printTable(int n)
-        {
+
+        // Time Complexity = O(1), Space Complexity = O(1)
+        void printTable(int n) {
             for(int i=10; i>=1; i--)
                 cout << n*i <<" ";
             cout<<endl;
         }  
 };
 
-// Time Complexity = O(1)
-// Space Complexity = O(1)
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+
+	while(t--) {
+	    int n;
+	    cin>>n;
+	    Solution obj;
+	    obj.printTable(n);	    
+	}
+
+	return 0;
+}
+// } Driver Code Ends
