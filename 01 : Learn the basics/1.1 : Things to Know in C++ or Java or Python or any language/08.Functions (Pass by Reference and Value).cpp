9e60ee8c3c81ec0163cf8daffdf6d02c31db1f5e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-04-02 17:14:25 +0000

    code added

--- 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/08.Functions (Pass by Reference and Value).cpp
+++ 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/08.Functions (Pass by Reference and Value).cpp
@@ -1,7 +1,15 @@
 // Link : https://practice.geeksforgeeks.org/problems/pass-by-reference-and-value/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pass-by-reference-and-value
 
+
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
   public:
+
+    // Time Complexity = O(1), Space Complexity = O(1)
     vector<int> passedBy(int a, int &b) {
         // code here
         
@@ -16,5 +24,22 @@ class Solution {
     }
 };
 
-// Time Complexity = O(1)
-// Space Complexity = O(1)
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        Solution obj;
+        vector<int> ans = obj.passedBy(a, b);
+        cout << ans[0] << " " << ans[1] << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
