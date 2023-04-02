a69c000a984e6ed50a0487fecc8549d38e781b0e
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-04-02 17:34:07 +0000

    code added

--- 01 : Learn the basics/1.4 : Know Basic Maths/2.Reverse_a_Number.cpp
+++ 01 : Learn the basics/1.4 : Know Basic Maths/2.Reverse_a_Number.cpp
@@ -1,44 +1,45 @@
 // Link - gfg : https://practice.geeksforgeeks.org/problems/reverse-bits3556/1
 
-/*class Solution {
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
   public:
+
+    // Time Complexity = O(logx), Space Complexity = O(1)
     long long reversedBits(long long x) {
         // code here
         long long sum = 0;
         int p = 31;
         
-        while(x != 0){
+        while(x != 0) {
             int r = x%2;
+            
             if(r == 1)
                 sum += pow(2, p);
+                
             x /= 2;
             p--;
         }
-        return sum;
-    }
-};*/
-// Time Complexity = O(logx)
-// Space Complexity = O(1)
-
-
-
-// Link - Leetcode : https://leetcode.com/problems/reverse-integer/submissions/
-
-class Solution {
-public:
-    int reverse(int x) {
-        int ans = 0;
         
-        while(x != 0) {
-            int d = x%10;
-            if(ans < INT_MIN/10 || ans > INT_MAX/10)
-                return 0;
-            ans = (ans * 10) + d;
-            x /= 10;
-        }
-        return ans;
+        return sum;
     }
 };
 
-// Time Complexity = O(logx)
-// Space Complexity = O(1)
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long X;
+        
+        cin>>X;
+
+        Solution ob;
+        cout << ob.reversedBits(X) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
