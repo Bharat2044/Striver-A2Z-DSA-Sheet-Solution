4eac7a6cd4dcb3edec84258b657114d8ddf03cfc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-04-02 17:34:03 +0000

    code added

--- 01 : Learn the basics/1.4 : Know Basic Maths/1.Count_Digits.cpp
+++ 01 : Learn the basics/1.4 : Know Basic Maths/1.Count_Digits.cpp
@@ -1,21 +1,44 @@
 // Link : https://practice.geeksforgeeks.org/problems/count-digits5716/1
 
+
+
+#include<bits/stdc++.h> 
+using namespace std;
+
 class Solution{
 public:
-    int evenlyDivides(int n){
+
+    // Time Complexity = O(logn), Space Complexity = O(1)
+    int evenlyDivides(int n) {
         //code here
+
         int count = 0;
         int t = n;
         
         while(n != 0){
             int d = n%10;
+
             if(d!=0 && t%d==0)
                 count++;
             n /= 10;
         }
+
         return count;
     }
 };
 
-// Time Complexity = O(logn)
-// Space Complexity = O(1)
+//{ Driver Code Starts.
+int main()  { 
+    int t;
+    cin>>t;
+
+    while(t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        cout << ob.evenlyDivides(N) << endl;
+    }
+
+    return 0; 
+}
+// } Driver Code Ends
