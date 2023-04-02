69215ce4f77f8a7160d205e556e1066305de426d
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-04-02 17:13:58 +0000

    code added

--- 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/03.If_Else_statements.cpp
+++ 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/03.If_Else_statements.cpp
@@ -1,7 +1,17 @@
 // Link : https://practice.geeksforgeeks.org/problems/java-if-else-decision-making0924/0?category%5B%5D=Java&category%5B%5D=Java&difficulty%5B%5D=-2&page=1&query=category%5B%5DJavadifficulty%5B%5D-2page1category%5B%5DJava
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
 class Solution {
   public:
+
+    // Time Complexity = O(1), Space Complexity = O(1)
     string compareNM(int n, int m){
         // code here
         if(n == m)
@@ -13,5 +23,17 @@ class Solution {
     }
 };
 
-// Time Complexity = O(1)
-// Space Complexity = O(1)
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n,m ; cin>>n>>m;
+        Solution obj;
+        cout<<obj.compareNM(n,m)<<"\n";
+    }
+
+    return 0;
+}
