f29b9c806c44458ef8499963f6e28b0080c3ed25
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-04-02 17:13:49 +0000

    code added

--- 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/01.User_Input_Output.cpp
+++ 01 : Learn the basics/1.1 : Things to Know in C++ or Java or Python or any language/01.User_Input_Output.cpp
@@ -1,12 +1,30 @@
 // Link : https://practice.geeksforgeeks.org/problems/c-input-output2432/1
 
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
 class Solution{   
 public:
-    int multiplication(int A, int B){
+
+    // Time Complexity = O(1), Space Complexity = O(1)
+    int multiplication(int A, int B) {
         // code here 
         return A*B;
     }
 };
 
-// Time Complexity = O(1)
-// Space Complexity = O(1)
+//{ Driver Code Starts.
+int main() { 
+    int t;
+    cin>>t;
+    
+    while(t--) {
+        int A, B;
+        cin >> A >> B;
+        Solution ob;
+        cout << ob.multiplication(A,B) << endl;
+    }
+
+    return 0; 
+} 
