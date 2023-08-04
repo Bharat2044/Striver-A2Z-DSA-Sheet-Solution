a20dab996a5af8d5f79fa8283ffbc0bb3c1e16a7
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-04 17:24:22 +0000

    code added

--- 01 : Learn the basics/1.4 : Know Basic Maths/5.Armstrong_Numbers.cpp
+++ 01 : Learn the basics/1.4 : Know Basic Maths/5.Armstrong_Numbers.cpp
@@ -1,8 +1,32 @@
-// Link - gfg : https://practice.geeksforgeeks.org/problems/armstrong-numbers2727/1
+// Question Link: https://www.codingninjas.com/studio/problems/check-armstrong_589?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+bool checkArmstrong(int n){
+	//Write your code here
+	int count = 0;
+	int x = n, y = n;
+
+	while(n > 0) {
+		count++;
+		n /= 10;
+	}
+
+	int sum = 0;
+	while(x > 0) {
+		int d = x%10;
+		int power = pow(d, count);
+		
+		sum += power;
+
+		x /= 10;
+	}
+
+	return sum == y;
+}
 
 
 
 
+
+// Question Link - gfg : https://practice.geeksforgeeks.org/problems/armstrong-numbers2727/1
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -12,20 +36,20 @@ class Solution {
     // Time Complexity = O(logn), Space Complexity = O(1)
     string armstrongNumber(int n){
         // code here
-        // int t=n;
-        // int x=n;
-        // int count=0;
-        // while(t!=0){
-        //     count++;
-        //     t/=10;
-        // }
-        // int sum=0;
-        // while(n!=0){
-        //     int d=n%10;
-        //     int power=pow(d,count);
-        //     sum += power;
-        // }
-        // return sum==x?"Yes":"No";
+        /*int t=n;
+        int x=n;
+        int count=0;
+        while(t!=0){
+            count++;
+            t/=10;
+        }
+        int sum=0;
+        while(n!=0){
+            int d=n%10;
+            int power=pow(d,count);
+            sum += power;
+        }
+        return sum==x?"Yes":"No";*/
         
         int num = n;
         int sum=0;
