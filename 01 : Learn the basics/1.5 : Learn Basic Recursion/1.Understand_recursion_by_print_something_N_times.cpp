aa70ffd5e1c8276562710c32e8f225c16e57bf7c
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-04 17:44:27 +0000

    code added

--- 01 : Learn the basics/1.5 : Learn Basic Recursion/1.Understand_recursion_by_print_something_N_times.cpp
+++ 01 : Learn the basics/1.5 : Learn Basic Recursion/1.Understand_recursion_by_print_something_N_times.cpp
@@ -1,7 +1,27 @@
+// Question Link: https://www.codingninjas.com/studio/problems/print-1-to-n_628290?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+void solve(vector<int> &arr, int x) {
+    if(x == 1) {
+        arr.push_back(1);
+        return;
+    }
+    else {
+        solve(arr, x-1);
+        arr.push_back(x);
+    }    
+}
+
+vector<int> printNos(int x) {
+    // Write Your Code Here
+    vector<int> arr;
+    solve(arr, x);
+
+    return arr;
+}
+
+
+
+
 // Question Link : https://practice.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print-1-to-n-without-using-loops
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
