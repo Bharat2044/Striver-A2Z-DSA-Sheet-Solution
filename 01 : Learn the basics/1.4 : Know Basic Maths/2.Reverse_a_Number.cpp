56b9acfd7f3c869c76ba791fb25392d8f5db085b
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-04 17:24:13 +0000

    code added

--- 01 : Learn the basics/1.4 : Know Basic Maths/2.Reverse_a_Number.cpp
+++ 01 : Learn the basics/1.4 : Know Basic Maths/2.Reverse_a_Number.cpp
@@ -1,7 +1,48 @@
-// Link - gfg : https://practice.geeksforgeeks.org/problems/reverse-bits3556/1
+// Question Link : https://www.codingninjas.com/studio/problems/reverse-bits_2181102?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+long reverseBits(long n) {
+    // Write your code here.
+    unsigned long ans = 0;
+    int i = 0;
+    unsigned int one = 1;
+
+    while(n){
+        int bit = n & 1;
+        if(bit)
+            ans = ans | (one<<(31-i));
+        
+        n >>= 1;
+        i++;
+    }
+
+    return ans;
+}
 
 
 
+// Question Link : https://leetcode.com/problems/reverse-integer/
+class Solution {
+public:
+    int reverse(int n) {
+        int rev = 0;
+        
+        while(n) {
+            int d = n%10;
+            
+            if(rev>INT_MAX/10 || rev<INT_MIN/10)
+                return 0;
+            
+            rev = (rev*10)+d;
+            n /= 10;
+        }
+        
+        return rev;
+    }
+};
+
+
+
+
+// Question Link - gfg : https://practice.geeksforgeeks.org/problems/reverse-bits3556/1
 #include <bits/stdc++.h>
 using namespace std;
 
