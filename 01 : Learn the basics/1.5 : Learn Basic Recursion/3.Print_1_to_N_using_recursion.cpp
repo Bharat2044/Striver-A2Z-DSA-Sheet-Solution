e25674b66ecb1093caa82a69e646e4c3ffd6b7f0
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-09 15:54:25 +0000

    code added

--- 01 : Learn the basics/1.5 : Learn Basic Recursion/3.Print_1_to_N_using_recursion.cpp
+++ 01 : Learn the basics/1.5 : Learn Basic Recursion/3.Print_1_to_N_using_recursion.cpp
@@ -1,8 +1,24 @@
+// Question Link: https://www.codingninjas.com/studio/problems/print-1-to-n_628290?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+// Time Complexity = O(n), Space Complexity = O(n)
+void solve(vector<int> &output, int x) {
+    if(x == 0) 
+        return;
+    
+    solve(output, x-1);
+    output.push_back(x);
+}
+
+vector<int> printNos(int x) {
+    // Write Your Code Here
+    vector<int> output;
+    solve(output, x);
+
+    return output;
+}
+
+
+
 // Question Link: https://practice.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print_1_to_n_without_loop
-
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
