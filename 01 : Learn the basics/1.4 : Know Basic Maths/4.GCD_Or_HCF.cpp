42ec193ef380add9e230bc87f2411989e6087431
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-09 15:29:29 +0000

    code added

--- 01 : Learn the basics/1.4 : Know Basic Maths/4.GCD_Or_HCF.cpp
+++ 01 : Learn the basics/1.4 : Know Basic Maths/4.GCD_Or_HCF.cpp
@@ -1,7 +1,7 @@
 // Question Link : https://www.codingninjas.com/studio/problems/hcf-and-lcm_840448?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
 int calcGCD(int n, int m){
     // Write your code here.
-    if(m==0) 
+    if(m == 0) 
         return n;
 
     return calcGCD(m, n%m);
