f8a8b62d3d28f8f2c9dd15bcf7cca91939fb2ccc
Bharat Kumar <104676718+Bharat2044@users.noreply.github.com>
2023-08-09 15:42:27 +0000

    code added

--- 01 : Learn the basics/1.5 : Learn Basic Recursion/2.Print_name_N_times_using_recursion.cpp
+++ 01 : Learn the basics/1.5 : Learn Basic Recursion/2.Print_name_N_times_using_recursion.cpp
@@ -1,7 +1,22 @@
+// Question Link: https://www.codingninjas.com/studio/problems/-print-n-times_8380707?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0
+void solve(vector<string> &output, int x) {
+    if(x == 0) 
+        return;
+    
+    solve(output, x-1);
+    output.push_back("Coding Ninjas"); 
+}
+
+vector<string> printNTimes(int n) {
+	// Write your code here.
+	vector<string> output;
+	solve(output, n);
+
+	return output;
+}
+
+
 // Question Link : https://practice.geeksforgeeks.org/problems/print-gfg-n-times/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=print-gfg-n-times
-
-
-
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -28,9 +43,6 @@ class Solution {
     }
 };
 
-
-//{ Driver Code Starts.
-/* Driver program to test printNos */
 int main() {
     int T;
 
